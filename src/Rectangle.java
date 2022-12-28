78031c60505e0df1e5da6e1d1c69066c08812387
kiran_a <iamkiranashok@gmail.com>
2022-12-28 15:41:14 +0530

    Second commit

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle {
+    public Double Length,Width,Area,Perimeter;
+
+    public Rectangle(Double Length,Double Width )
+    {
+this.Length=Length;
+this.Width=Width;
+    }
+    public Double getLength()
+    {
+        return this.Length;
+    }
+    public Double getWidth()
+    {return this.Width;}
+    public Double getArea()
+
+    {
+        return Area=Length*Width;
+    }
+    public Double getPerimeter()
+    {return Perimeter=2*(Length+Width);}
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "Length=" + Length +
+                ", Width=" + Width +
+                ", Area=" + Area +
+                ", Perimeter=" + Perimeter +
+                '}';
+    }
 }
--- src/TestRectangle.java
+++ src/TestRectangle.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TestRectangle {
+public class TestRectangle {
+    public static void main(String[] args)
+    {
+        Rectangle r= new Rectangle(2.0,2.0);
+        {
+r.getArea();
+r.getPerimeter();
+System.out.println("The result is " + r);
+        }
+    }
 }
